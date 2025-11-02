a93f0169187ffd3d14ed61829441b8c466c80170
Taylor <meisterttl@gmail.com>
2025-11-02 01:24:50 -0700

    Vite config file update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,4 +6,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Dice-Poker/",
+  assestsInclude: ["**/*.wasm.wasm"],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
