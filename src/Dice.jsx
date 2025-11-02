d06210794ce1f679d473bf7fcc1f62cd52c72f2d
Taylor <meisterttl@gmail.com>
2025-11-02 01:30:41 -0800

    Fixing assetPath in Dice.jsx

--- src/Dice.jsx
+++ src/Dice.jsx
@@ -2,7 +2,7 @@ import DiceBox from "@3d-dice/dice-box";
 
 const Dice = new DiceBox({
   id: "dice-canvas",
-  assetPath: "/src/assets/",
+  assetPath: "/assets/",
   friction: 1,
   restitution: 0.6,
   angularDamping: 0.6,
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Dice-Poker/",
-  assestsInclude: ["**/*.wasm.wasm"],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
 });
